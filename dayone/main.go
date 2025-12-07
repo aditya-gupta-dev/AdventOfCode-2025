4cfb627544fab85bf2edc8c4da8d57254fc02fee
Aditya Gupta <adityagupta2087@gmail.com>
2025-12-08 00:39:52 +0530

    completed d1p2

--- dayone/main.go
+++ dayone/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
@@ -44,6 +45,57 @@ func PartOne() {
 	fmt.Println(zeroCounter)
 }
 
+func PartTwo() {
+	data, err := os.ReadFile("input-one.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	rotation := 50
+	zeroCounter := 0
+
+	scanner := bufio.NewScanner(strings.NewReader(string(data)))
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+
+		dir := line[0]
+		degree, err := strconv.Atoi(line[1:])
+		if err != nil {
+			panic(err)
+		}
+
+		switch dir {
+		case 'R':
+			zeroCounter += (degree + rotation) / 100
+			rotation = (rotation + degree) % 100
+
+		case 'L':
+			if rotation == 0 {
+				zeroCounter += degree / 100
+			} else {
+				zeroCounter += (degree + (100 - rotation)) / 100
+			}
+
+			rotation = (rotation - degree) % 100
+			if rotation < 0 {
+				rotation += 100
+			}
+		default:
+			panic("invalid direction: " + line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(zeroCounter)
+}
+
 func main() {
 	PartOne()
+	PartTwo()
 }
