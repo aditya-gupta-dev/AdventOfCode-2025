1d6f5bf1ff607fcdda1cb30c88cbb6444b54ce52
Aditya Gupta <adityagupta2087@gmail.com>
2025-12-13 12:35:45 +0530

    completed d3p1

--- daythree/main.go
+++ daythree/main.go
@@ -2,17 +2,61 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 )
 
 func PartOne() {
-	f, err := os.Open("input.txt")
+	parseBatteries := func(line string) []int {
+		var batteries []int = make([]int, len(line))
+		for i, text := range line {
+			battery, err := strconv.Atoi(string(text))
+			if err != nil {
+				panic(err)
+			}
+			batteries[i] = battery
+		}
+		return batteries
+	}
+
+	solveBatteries := func(batteries []int) int {
+		var maxJoltage int = 0
+		var jolt int = 0
+
+		for i := 0; i < len(batteries)-1; i++ {
+			firstDigit := batteries[i]
+			var secondDigit int = batteries[i+1]
+			for j := i + 2; j < len(batteries); j++ {
+				if batteries[j] > secondDigit {
+					secondDigit = batteries[j]
+				}
+			}
+			jolt = firstDigit*10 + secondDigit
+			if maxJoltage < jolt {
+				maxJoltage = jolt
+			}
+		}
+		return maxJoltage
+	}
+
+	file, err := os.Open("input-three.txt")
 	if err != nil {
 		panic(err)
 	}
-	bufio.NewReader()
+	defer file.Close()
+
+	var total int = 0
+	var scanner = bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		var batteries = scanner.Text()
+		total += solveBatteries(parseBatteries(batteries))
+	}
+
+	fmt.Println(total)
 }
 
 func main() {
-
+	PartOne()
 }
