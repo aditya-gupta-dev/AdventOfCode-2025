69f699367a6a396fdeed9aaea049c7bc5cd7add4
Aditya Gupta <adityagupta2087@gmail.com>
2026-07-01 19:57:39 +0530

    day 4 part 2

--- dayfour/src/main.rs
+++ dayfour/src/main.rs
@@ -1,4 +1,4 @@
-use std::{char, fs, usize};
+use std::fs;
 
 struct Solution {}
 
@@ -8,16 +8,16 @@ struct Ind {
 }
 
 fn get_adjacent_indicies(x: i32, y: i32) -> Vec<Ind> {
-    let mut res = Vec::new();
-    res.push(Ind { x: x - 1, y: y - 1 });
-    res.push(Ind { x: x, y: y - 1 });
-    res.push(Ind { x: x + 1, y: y - 1 });
-    res.push(Ind { x: x - 1, y: y });
-    res.push(Ind { x: x + 1, y: y });
-    res.push(Ind { x: x - 1, y: y + 1 });
-    res.push(Ind { x: x, y: y + 1 });
-    res.push(Ind { x: x + 1, y: y + 1 });
-    return res;
+    vec![
+        Ind { x: x - 1, y: y - 1 },
+        Ind { x: x, y: y - 1 },
+        Ind { x: x + 1, y: y - 1 },
+        Ind { x: x - 1, y: y },
+        Ind { x: x + 1, y: y },
+        Ind { x: x - 1, y: y + 1 },
+        Ind { x: x, y: y + 1 },
+        Ind { x: x + 1, y: y + 1 },
+    ]
 }
 
 fn is_valid_index(x: i32, y: i32, limits: &Ind) -> bool {
@@ -30,19 +30,25 @@ fn is_valid_index(x: i32, y: i32, limits: &Ind) -> bool {
     true
 }
 
+fn count_adjacent_paper(grid: &Vec<Vec<char>>, x: i32, y: i32, limits: &Ind) -> i32 {
+    let mut count = 0;
+    for adj_ind in get_adjacent_indicies(x, y) {
+        if is_valid_index(adj_ind.x, adj_ind.y, limits) {
+            if grid[adj_ind.y as usize][adj_ind.x as usize] == '@' {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
 impl Solution {
     fn part_one(file_name: String) -> i32 {
         let data: String = String::from_utf8(fs::read(file_name).unwrap()).unwrap();
         let mut grid: Vec<Vec<char>> = Vec::new();
-        let paper: char = '@';
-        let mut forkable = 0;
 
         for line in data.lines() {
-            let mut row: Vec<char> = Vec::new();
-            for item in line.chars() {
-                row.push(item);
-            }
-            grid.push(row);
+            grid.push(line.chars().collect());
         }
 
         let limits: Ind = Ind {
@@ -50,19 +56,11 @@ impl Solution {
             y: grid.len() as i32,
         };
 
-        for (y, _) in grid.iter().enumerate() {
-            for (x, _) in grid.iter().enumerate() {
-                if grid[y][x] == paper {
-                    let adjacent_indicies = get_adjacent_indicies(x as i32, y as i32);
-                    let mut adjacent_paper_count = 0;
-                    for adj_ind in adjacent_indicies {
-                        if is_valid_index(adj_ind.x, adj_ind.y, &limits) {
-                            if grid[adj_ind.y as usize][adj_ind.x as usize] == paper {
-                                adjacent_paper_count += 1;
-                            }
-                        }
-                    }
-
+        let mut forkable = 0;
+        for y in 0..grid.len() {
+            for x in 0..grid[y].len() {
+                if grid[y][x] == '@' {
+                    let adjacent_paper_count = count_adjacent_paper(&grid, x as i32, y as i32, &limits);
                     if adjacent_paper_count < 4 { 
                         forkable += 1; 
                     }
@@ -73,10 +71,55 @@ impl Solution {
         forkable
     }
 
+    fn part_two(file_name: String) -> i32 {
+        let data: String = String::from_utf8(fs::read(file_name).unwrap()).unwrap();
+        let mut grid: Vec<Vec<char>> = Vec::new();
+
+        for line in data.lines() {
+            grid.push(line.chars().collect());
+        }
+
+        let limits: Ind = Ind {
+            x: grid[0].len() as i32,
+            y: grid.len() as i32,
+        };
+
+        let mut total_removed = 0;
+
+        loop {
+            // Find all removable rolls in current state
+            let mut to_remove: Vec<(usize, usize)> = Vec::new();
+            
+            for y in 0..grid.len() {
+                for x in 0..grid[y].len() {
+                    if grid[y][x] == '@' {
+                        let adjacent_paper_count = count_adjacent_paper(&grid, x as i32, y as i32, &limits);
+                        if adjacent_paper_count < 4 {
+                            to_remove.push((y, x));
+                        }
+                    }
+                }
+            }
+
+            // If no more rolls can be removed, we're done
+            if to_remove.is_empty() {
+                break;
+            }
+
+            // Remove all removable rolls
+            for (y, x) in to_remove {
+                grid[y][x] = '.';
+                total_removed += 1;
+            }
+        }
+
+        total_removed
+    }
 }
 
 
 fn main() {
     let file_name: String = String::from("input-four.txt");
-    println!("{}", Solution::part_one(file_name))
+    println!("Part 1: {}", Solution::part_one(file_name.clone()));
+    println!("Part 2: {}", Solution::part_two(file_name));
 }
